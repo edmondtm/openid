a059ec870cabb977583c6b4e52e213e01ae7ae30
edmondtm <edmondtm@gmail.com>
2022-03-09 20:02:58 +0800

    forget password

--- apps/magic-staging/src/app/forget/forget/forget.component.ts
+++ apps/magic-staging/src/app/forget/forget/forget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'openid-forget',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgetComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onSignup() {
+    this.router.navigate(['/signup']);
+  }
 }
